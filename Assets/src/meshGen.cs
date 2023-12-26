7bf4b388623949d1d981672bc7ed7c84970dff98
James <10james.maxwell@gmail.com>
2023-12-26 20:07:29 +0000

    First commit

---
+++ Assets/src/meshGen.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System;
+
+public class MeshGen : MonoBehaviour
+{
+    public static Mesh create(int linearRes, float xLen, float yLen, Vector3 position)
+    {
+        Mesh mesh = new Mesh();
+
+        float xStep = xLen / linearRes;
+        float yStep = yLen / linearRes;
+        Vector3[] vertices = new Vector3[linearRes*linearRes];
+
+        for (int i=0; i<linearRes; i++)
+        {
+            float xPos = position[0]+i*xStep;
+            for (int j=0; j<linearRes; j++) {
+                float yPos = position[1]+j*yStep;
+                vertices[i*linearRes+j] = new Vector3(xPos, yPos, 0);
+            }
+        }
+
+        int numIndices = (linearRes-1)*(linearRes-1)*2*3;
+        int[] indices = new int[numIndices];
+        int count =0 ;
+        for (int i=0; i<linearRes-1; i++) {
+            for (int j=0; j<linearRes-1; j++) {
+                indices[count] = i*linearRes + j;
+                indices[count+1] = i*linearRes + j+1;
+                indices[count+2] = (i+1)*linearRes+j;
+
+                indices[count+3] = linearRes*linearRes - (i*linearRes + j) - 1;
+                indices[count+4] = linearRes*linearRes - (i*linearRes + j+1) - 1;
+                indices[count+5] = linearRes*linearRes - ((i+1)*linearRes+j) - 1;
+                count += 6;
+            }
+        }
+
+        Vector3[] normals = new Vector3[linearRes*linearRes];
+        Array.Fill(normals, -Vector3.forward);
+
+        mesh.vertices = vertices;
+        mesh.triangles = indices;
+        mesh.normals = normals;
+
+        return mesh;
+    }
+}
---
+++ Assets/src/test.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class QuadCreator : MonoBehaviour
+{
+    public int linearRes = 50;
+    public float xLen = 1;
+    public float yLen = 1;
+    public Vector3 position = new Vector3(0, 0, 0);
+
+    public void Start()
+    {
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+
+        Mesh mesh = MeshGen.create(linearRes, xLen, yLen, position);
+        meshFilter.mesh = mesh;
+    }
+}
